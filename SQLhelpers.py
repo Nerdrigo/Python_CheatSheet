e2d2f330f99be19186ac2ccc38d16a56a0b6f9df
Rodrigo <30826382+Nerdrigo@users.noreply.github.com>
2018-08-27 11:30:03 -0500

    Update SQLhelpers.py

--- SQLhelpers.py
+++ SQLhelpers.py
@@ -10,17 +10,9 @@ PG_DB = os.environ['PG_DB']      #Database name
 PG_PORT = os.environ['PG_PORT']  #Port number
 
 
-def get_postgres_connection():
-    '''
-    This function connects to a database
-    There needs to be an existing connection
-    '''
-    con = psycopg2.connect(
-        "dbname={} user={} password={} host={} port={}".format(
-            PG_DB, PG_USR, PG_PASS,
-            PG_HOST, PG_PORT
-        )
-    )
+def get_db_data():
+    con = psycopg2.connect(host=PG_HOST, user=PG_USR,
+                           password=PG_PASS, dbname=PG_DB)
     return con
 
 
